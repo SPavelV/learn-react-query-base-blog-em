9787d571c98d4877810a3b1ff4f1b74bf8655de6
Pavel Solovev <p.solovev@articul.ru>
2021-12-04 12:38:22 +0400

    Create Query for Blog Comments

--- src/PostDetail.jsx
+++ src/PostDetail.jsx
@@ -1,3 +1,5 @@
+import { useQuery } from "react-query";
+
 async function fetchComments(postId) {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/comments?postId=${postId}`
@@ -23,7 +25,10 @@ async function updatePost(postId) {
 
 export function PostDetail({ post }) {
   // replace with useQuery
-  const data = [];
+  const { data, isError, error, isLoading } = useQuery(
+    ["comment", post?.id],
+    () => fetchComments(post.id)
+  );
 
   return (
     <>
@@ -31,7 +36,7 @@ export function PostDetail({ post }) {
       <button>Delete</button> <button>Update title</button>
       <p>{post.body}</p>
       <h4>Comments</h4>
-      {data.map((comment) => (
+      {data?.map((comment) => (
         <li key={comment.id}>
           {comment.email}: {comment.body}
         </li>
